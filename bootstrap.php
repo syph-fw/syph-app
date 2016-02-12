675bbdcb8632f4b9ffcc6102f960743cb7ec133d
btlouvem <bruno@prapagar.com>
2016-02-12 11:24:29 -0200

    Clean assets in app-example

--- bootstrap.php
+++ bootstrap.php
@@ -9,9 +9,9 @@
 //Include Autoload
 require_once 'autoload.php';
 
-include_once('global/functions.php');
+require_once('global/functions.php');
 
-include_once('routing.php');
+require_once('routing.php');
 
 require_once USER_APPS_DIR.'AppKernel.php';
 
