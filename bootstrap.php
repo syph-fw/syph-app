f6d1656f4bb0eb0917baa8633acd16a88cfcbc59
btlouvem <bruno@prapagar.com>
2016-02-12 10:42:56 -0200

    Refactory index and bootstrap in application

--- bootstrap.php
+++ bootstrap.php
@@ -7,21 +7,13 @@
  */
 
 //Include Autoload
-require_once __DIR__.'/vendor/autoload.php';
+require_once 'autoload.php';
 
-$env = include_once('env.php');
 include_once('global/functions.php');
-$loaded = new \Syph\AppBuilder\Environment();
-$loaded->setEnv($env);
-
-//Start Routes
-foreach($env['packages'] as $name => $path) {
-    $loader = new \Syph\Autoload\ClassLoader($name, $path);
-    $loader->register();
-    $loaded->setLoaded($loader);
-}
 
 include_once('routing.php');
 
-return $loaded;
+require_once USER_APPS_DIR.'AppKernel.php';
+
+
 
